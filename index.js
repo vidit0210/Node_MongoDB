81e9df01ad957ec68e9a7d14061e6cb7a8e640ed
Vidit Shah <vidit0210@gmail.com>
2018-11-16 01:04:26 +0530

    Schema Declaration

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 //Mongoose is an API Require to interact with our MongoDB Data Base.
 //Mongoose.Connect returns a Promise
 
-const dbdebug = require('debug')('app:dbdebugger');
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
-    .then(()=>{console.log("Connecting to The Database")});
+    .then(()=>{console.log('Connected to db')})
+    .catch(err=>console.log('Error Connecting'));
+
+const courseSchema  = new mongoose.Schema({
+    name:String,
+    author:String,
+    tags:[String],
+    date:{type :Date,default:Date.now()},
+    isPublished:Boolean
+});
+//mongoose.model Gives back a Classs
+
+const Course  = mongoose.model('Course',courseSchema);
+const course = new Course({
+    name:"Python",
+    author: "Vidit Shah",
+    tags:['Scripting Languages','Trending'],
+    isPublished: true
+});
