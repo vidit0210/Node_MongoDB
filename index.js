7bb02b0a2cf5ee28fa3d892d75b29bf0ca7d06cd
Vidit Shah <vidit0210@gmail.com>
2018-11-16 01:27:08 +0530

    Querying the Mongo DataBase

--- index.js
+++ index.js
@@ -17,9 +17,31 @@ const courseSchema  = new mongoose.Schema({
 //mongoose.model Gives back a Classs
 
 const Course  = mongoose.model('Course',courseSchema);
-const course = new Course({
-    name:"Python",
-    author: "Vidit Shah",
-    tags:['Scripting Languages','Trending'],
-    isPublished: true
-});
+
+async function createCourse() {
+    const course = new Course({
+        name:"Angular",
+        author: "Vidit Shah",
+        tags:['Angular','Front End'],
+        isPublished: true
+    });
+
+//Saving is Aysch operation=It Gives promise
+    const result =  await course.save();
+    console.log(result);
+};
+
+//createCourse();
+async function getAllCourses() {
+
+    // const result =  await Course.find();
+
+    //Filtering the Query
+   // const result =  await Course.find({name:"Angular"});
+
+    //More Complex Filtering {} :1 for ascending -1 for descending
+    const result =  await Course.find({author:"Vidit Shah"}).sort({name:1});
+
+    console.log(result)
+}
+getAllCourses();
